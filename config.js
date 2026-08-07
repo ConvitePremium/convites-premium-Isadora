bc8c0c6a099c5f6513039e7b12c889be00e79fd1
ConvitePremium <essenciamusicalofc@gmail.com>
2026-08-07 16:12:06 -0300

    Update config.js

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ window.CONFIG = {
   //         Ex.: 55 (Brasil) + 31 (DDD) + 985657116 -> "5531985657116"
   // mensagem: texto que já vem pré-preenchido quando a pessoa abre o WhatsApp.
   whatsapp: {
-    numero: "5511948469479",
+    numero: "5511984510281",
     mensagem: "Olá! Confirmo minha presença no aniversário da Isadora."
   },
 
